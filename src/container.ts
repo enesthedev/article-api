fceb0bb245c73d1b9df6a38c49b72fafa3d06163
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-03-05 22:07:29 +0300

    Refactor: Add ArticleRepository, ArticleService and PrismaClientProvider to inversify container

--- src/container.ts
+++ src/container.ts
@@ -3,7 +3,17 @@ import 'reflect-metadata';
 import '@application/controllers';
 
 import { Container } from 'inversify';
+import { ArticleRepository } from '@domain/article/ArticleRepository';
+import { ArticleRepositoryImpl } from '@infrastructure/repository/ArticleRepositoryImpl';
+import { TYPES } from '@project/types';
+import { ArticleService } from './domain/article';
+import { ArticleServiceImpl } from './application/services/ArticleServiceImpl';
+import { PrismaClientProvider } from './infrastructure/database/prisma/PrismaClientProvider';
 
 const container = new Container();
 
+container.bind<ArticleRepository>(TYPES.ArticleRepository).to(ArticleRepositoryImpl);
+container.bind<ArticleService>(TYPES.ArticleService).to(ArticleServiceImpl);
+
+container.bind<PrismaClientProvider>(PrismaClientProvider).toSelf();
 export default container;
