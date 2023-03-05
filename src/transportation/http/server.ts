5145240155c383c26cc1907dc1af73e595ef00a0
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-03-05 14:43:02 +0300

    Add: InversifyExpressUtils server integration

---
+++ src/transportation/http/server.ts
@@ -0,0 +1,46 @@
+import express, { Application, Router } from 'express';
+
+import { Container } from 'inversify';
+import { AuthProvider, InversifyExpressServer, RoutingConfig } from 'inversify-express-utils';
+
+export interface IHttpServer {}
+
+export class HttpServer extends InversifyExpressServer implements IHttpServer {
+  private constructor(
+    container: Container,
+    customRouter?: Router | null,
+    routingConfig?: RoutingConfig | null,
+    customApp?: Application | null,
+    authProvider?: (new () => AuthProvider) | null,
+    forceControllers?: boolean
+  ) {
+    super(container, customRouter, routingConfig, customApp, authProvider, forceControllers);
+
+    this.setConfig(this.config);
+  }
+
+  private config(app: Application): void {
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+  }
+
+  public static create(
+    container: Container,
+    customRouter?: Router | null,
+    routingConfig?: RoutingConfig | null,
+    customApp?: Application | null,
+    authProvider?: (new () => AuthProvider) | null,
+    forceControllers?: boolean
+  ): HttpServer {
+    const server = new HttpServer(
+      container,
+      customRouter,
+      routingConfig,
+      customApp,
+      authProvider,
+      forceControllers
+    );
+
+    return server;
+  }
+}
