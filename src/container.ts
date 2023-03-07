f136c0d8cca4b23b5eb7258abaacc26b1cdcd968
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-03-07 08:29:28 +0300

    Add: RequestValidatorMiddleware to  IoC container

--- src/container.ts
+++ src/container.ts
@@ -1,14 +1,15 @@
 import 'reflect-metadata';
 
-import '@application/controllers';
+import '@application/controller';
 
 import { Container } from 'inversify';
 import { ArticleRepository } from '@domain/article/ArticleRepository';
 import { ArticleRepositoryImpl } from '@infrastructure/repository/ArticleRepositoryImpl';
 import { TYPES } from '@project/types';
 import { ArticleService } from './domain/article';
-import { ArticleServiceImpl } from './application/services/ArticleServiceImpl';
+import { ArticleServiceImpl } from './application/service/ArticleServiceImpl';
 import { PrismaClientProvider } from './infrastructure/database/prisma/PrismaClientProvider';
+import { RequestValidatorMiddleware } from './transportation/http/middleware/RequestValidatorMiddleware';
 
 const container = new Container();
 
@@ -16,4 +17,6 @@ container.bind<ArticleRepository>(TYPES.ArticleRepository).to(ArticleRepositoryI
 container.bind<ArticleService>(TYPES.ArticleService).to(ArticleServiceImpl);
 
 container.bind<PrismaClientProvider>(PrismaClientProvider).toSelf();
+
+container.bind<RequestValidatorMiddleware>(RequestValidatorMiddleware).toSelf().inSingletonScope();
 export default container;
