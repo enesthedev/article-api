987704c989c8d405380e7aeab1700754c32d7c48
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-03-09 17:05:25 +0300

    Refactor PrismaClientProvider
    - Fixed creation bug of PrismaClient infinite times.

--- src/infrastructure/database/prisma/PrismaClientProvider.ts
+++ src/infrastructure/database/prisma/PrismaClientProvider.ts
@@ -3,13 +3,9 @@ import { injectable } from 'inversify';
 
 @injectable()
 export class PrismaClientProvider {
-  private readonly prisma: PrismaClient;
-
-  constructor() {
-    this.prisma = new PrismaClient({ errorFormat: 'minimal' });
-  }
+  private static readonly client: PrismaClient = new PrismaClient({ errorFormat: 'minimal' });
 
   public getPrismaClient(): PrismaClient {
-    return this.prisma;
+    return PrismaClientProvider.client;
   }
 }
